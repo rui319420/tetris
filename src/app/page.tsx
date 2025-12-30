f93f0dfbcb9242c46ea440394c83f1ffd1823ef2
Rui <s1F102501866@iniad.org>
2025-12-30 16:50:22 +0900

    feat: ボードにグリッドを入れるなど

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,19 @@ import styles from './page.module.css';
 const rows = 20,
   cols = 10;
 
+const BOARD = Array(cols * rows).fill(0);
+
 console.log(MINO.O);
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.board} /> */}
+      {}
+      <div className={styles.board}>
+        {BOARD.map((row, index) => (
+          <div key={index} className={styles.cell} />
+        ))}
+      </div>
       <div className={'mino'}>
         {MINO.I.map((row, y) => (
           <div key={y} style={{ display: 'flex' }}>
@@ -18,7 +25,7 @@ export default function Home() {
               if (cell === 1) {
                 return <div key={x} className={styles.block} />;
               } else {
-                return <div key={x}>a</div>;
+                return <div key={x} className={styles.empty} />;
               }
             })}
           </div>
